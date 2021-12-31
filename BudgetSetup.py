95955ce844ed870f6db6263f60fda04739b990eb
joc-gh <94961352+joc-gh@users.noreply.github.com>
2021-12-31 17:17:49 +0000

    Minor tweaks
    Explanatory text and addition of non-neg function to inputs

--- BudgetSetup.py
+++ BudgetSetup.py
@@ -1,3 +1,6 @@
+from ClassBudget import non-neg
+
+# Check for presence of balance.txt file from scrpit being run previously
 try:
     f = open("balance.txt","r")
     newuser = False
@@ -5,11 +8,12 @@ try:
 except IOError:
     newuser = True
 
+# If the file doesn't exist then request starting values for budgets and save them
 if newuser == True:
     print("Welcome to Budget App! New user detected, please set up your starting budgets.")
-    c_budget = int(input("New Clothing budget (£): "))
-    e_budget = int(input("New Entertainment budget (£): "))
-    f_budget = int(input("New Food budget (£): "))
+    c_budget = non-neg("New Clothing budget (£): "))
+    e_budget = non-neg("New Entertainment budget (£): "))
+    f_budget = non-neg("New Food budget (£): "))
     with open("balance.txt","w") as file:
         file.write(str(c_budget)+"\n")
         file.write(str(e_budget)+"\n")
